0dbd3ce7609f50b8699e5b91218747a25ec587aa
Frank Yu <frank@yuyuan.org>
2023-01-16 11:23:47 -0800

    recipe changes

--- public/program.js
+++ public/program.js
@@ -1,7 +1,9 @@
 function toggleShowHide() {
     if (document.getElementById("fwends").style.display == "none") {
         document.getElementById("fwends").style.display = ""
+        document.getElementById("button").innerHTML = "Hide"
     } else if (document.getElementById("fwends").style.display == "") {
         document.getElementById("fwends").style.display = "none"
+        document.getElementById("button").innerHTML = "Show"
     }
 }
