514631fe517293d23c1163e30d5d1d4c05ef1c9b
Frank Yu <frank@yuyuan.org>
2023-01-16 11:09:39 -0800

    recipe changes

---
+++ public/program.js
@@ -0,0 +1,7 @@
+function toggleShowHide() {
+    if (document.getElementById("fwends").style.display == "none") {
+        document.getElementById("fwends").style.display = ""
+    } else if (document.getElementById("fwends").style.display == "") {
+        document.getElementById("fwends").style.display = "none"
+    }
+}
